d74f6b0f1d5d1575acdddb5eb4760022296bab77
Akshat Sharma <akshatksharma@wustl.edu>
2020-11-21 00:18:42 -0600

    Added barebones add / remove functionality

--- exploreAllumin8/ViewController.swift
+++ exploreAllumin8/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
+    var db:Firestore?
+    
     override func viewDidLoad() {
+        self.db = Firestore.firestore()
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func insert(_ sender: Any) {
+        addToDb()
+    }
+    
+    
+    @IBAction func read(_ sender: Any) {
+        readFromDb()
+    }
+    
+    
+    func readFromDb() {
+        self.db?.collection("users").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                }
+            }
+        }
+    }
+    
+    func addToDb() {
+        
+        var ref: DocumentReference? = nil
+        ref = self.db?.collection("users").addDocument(data: [
+            "first": "Ada",
+            "last": "Lovelace",
+            "born": 1815
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+    }
+    
+    
 }
 

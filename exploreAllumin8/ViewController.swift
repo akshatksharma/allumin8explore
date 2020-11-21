5d9679d2b124ff3367fd00972675f35b8ab6189e
Akshat Sharma <akshatksharma@wustl.edu>
2020-11-20 21:53:39 -0600

    Initial Commit

---
+++ exploreAllumin8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  exploreAllumin8
+//
+//  Created by Akshat Sharma on 11/20/20.
+//  Copyright © 2020 Akshat Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
